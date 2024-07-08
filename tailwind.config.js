32b9ecbaf9ca3f108fdb22cf72f4495a795e5421
khurshid <Khurshid@qurilo.com>
2024-07-08 15:09:44 +0530

    update in tailwinnd config , index.html and hero component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Nunito Sans', 'ui-sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [],
 }
